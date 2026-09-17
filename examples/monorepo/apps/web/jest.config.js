2d91c96674dffd48845f89fe3ca1d713946eae14
gtkatakura <gt.katakura@gmail.com>
2026-09-17 00:09:16 -0300

    style: format Jest monorepo config

--- examples/monorepo/apps/web/jest.config.js
+++ examples/monorepo/apps/web/jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: 'node',
-  roots: ['<rootDir>/src/jest'], 
+  roots: ['<rootDir>/src/jest'],
   testMatch: ['**/*.spec.ts'],
   testTimeout: 60_000,
   // JSON report consumed by the e2e workflow
